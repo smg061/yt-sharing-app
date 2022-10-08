807f91ee3e4717a7f95b6cb7ac07cdd74608a9ac
smg061 <max.go.95@gmail.com>
2022-10-07 21:06:52 -0500

    shared queue works, I think!

---
+++ api/src/SocketService.ts
@@ -0,0 +1,10 @@
+
+
+export default {}
+
+export enum SOCKET_EVENT {
+    NEW_MESSAGE = "NEW_MESSAGE",
+    VIDEO_QUEUED = "VIDEO_QUEUED",
+    VIDEO_ENDED = "VIDEO_ENDED"
+} 
+
--- api/src/index.ts
+++ api/src/index.ts
@@ -2,11 +2,13 @@ import express, { Express, Request, Response } from "express";
 import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
-
+import { Queue } from "./Queue";
+import { SOCKET_EVENT } from "./SocketService";
 const app: Express = express();
 const server = http.createServer(app);
 const port = process.env.PORT || "3000";
 
+const { NEW_MESSAGE, VIDEO_QUEUED, VIDEO_ENDED } = SOCKET_EVENT;
 const io = new Server(server, {
   cors: {
     origin: "*",
@@ -14,17 +16,38 @@ const io = new Server(server, {
 });
 type Message = { user: string; userId: string; content: string };
 
-//cons messages: Message[] = []
+const videoQueue = new Queue<string>();
+let currentVideo: string;
 io.on("connection", (socket) => {
-  socket.on('message', (data: Message)=> {
-    io.emit('message',data)//.emit('message', data)
-  })
+  if(currentVideo) {
+    socket.emit(VIDEO_ENDED, currentVideo )
+  }
+  socket.on(NEW_MESSAGE, (data: Message) => {
+    io.emit(NEW_MESSAGE, data);
+  });
+  socket.on(VIDEO_QUEUED, (data: { previousUrl: string; newUrl: string }) => {
+    if (!data.previousUrl && data.newUrl!=='') {
+      io.emit(VIDEO_ENDED, data.newUrl);
+      currentVideo = data.newUrl
+      return
+    }
+    console.log("video received", data);
+    videoQueue.enqueue(data.newUrl);
+    io.emit(VIDEO_QUEUED, data.newUrl);
+  });
+  socket.on(VIDEO_ENDED, () => {
+    const url = videoQueue.dequeue();
+    if(typeof url !=='undefined') {
+      currentVideo = url;
+      io.emit(VIDEO_ENDED, url);
+    }
+  });
   socket.on("disconnect", (data) => console.log(data));
 });
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.CLIENT_URI || "*",
   })
 );
 
