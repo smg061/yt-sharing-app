3149a4beb64023bcea070fb7b836e945445e5bb1
Santos Gonzalez <max.go.95@gmail.com>
2022-10-31 21:08:15 -0500

    does hash routing work???

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { SocketProvider } from "./hooks/useWebSocket";
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link } from "react-router-dom";
 import "./index.css";
 import Room from "./routes/Room";
 
