dfb86cbbc0a6976dd4da5a96f5104d45da885856
smg061 <max.go.95@gmail.com>
2023-05-01 17:46:52 -0500

    more adjustment

--- apps/front-end/src/components/3D/Effects.tsx
+++ apps/front-end/src/components/3D/Effects.tsx
@@ -1,19 +1,15 @@
-import { extend } from "@react-three/fiber";
-import RetroLoader from "../loaders/RetroLoader";
-import Petal from "./Sakura";
+
 import {
-    Bloom,
-    DepthOfField,
+
     EffectComposer,
     Noise,
     Vignette,
     Glitch,
     ChromaticAberration,
     Scanline,
-    // @ts-ignore
 } from "@react-three/postprocessing";
 import { BlendFunction, GlitchMode } from "postprocessing";
-
+import { Vector2 } from "three";
 
 export const Effects = ()=> {
 
@@ -28,22 +24,24 @@ export const Effects = ()=> {
           height={480}
         /> */}
         <Glitch
-          delay={[2.5, 5.5]} // min and max glitch delay
-          duration={[0.3, 1.0]} // min and max glitch duration
-          strength={[0.1, 0.4]} // min and max glitch strength
+          delay={new Vector2(2.5, 5.5)} // min and max glitch delay
+          duration={new Vector2(0.3, 1.0)} // min and max glitch duration
+          strength={new Vector2(0.1, 0.4)} // min and max glitch strength
           mode={GlitchMode.SPORADIC} // glitch mode
           active // turn on/off the effect (switches between "mode" prop and GlitchMode.DISABLED)
           ratio={0.15}
         />
-        <Scanline
+        {/* <Scanline
           blendFunction={BlendFunction.OVERLAY} // blend mode
+          opacity={100} // opacity of the effect
           density={1.25} // scanline density
-        />
+        /> */}
         {/* <ChromaticAberration
           blendFunction={BlendFunction.NORMAL} // blend mode
           offset={[0.02, 0.002]} // color offset
         /> */}
         {/* <Bloom luminanceThreshold={0} luminanceSmoothing={0.9} height={300} /> */}
+        {/* @ts-ignore */}
         <Noise opacity={1} />
         {/* <Vignette eskil={false} offset={0.1} darkness={0.1} /> */}
         {/* <Pixelation/> */}
